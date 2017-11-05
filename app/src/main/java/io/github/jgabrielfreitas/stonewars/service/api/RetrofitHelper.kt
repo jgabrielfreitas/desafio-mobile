1cfa7a41b1efd2f00c2285519689cea713669aae
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-11-05 19:30:35 -0200

    okhttp client instance

--- app/src/main/java/io/github/jgabrielfreitas/stonewars/service/api/RetrofitHelper.kt
+++ app/src/main/java/io/github/jgabrielfreitas/stonewars/service/api/RetrofitHelper.kt
@@ -1,6 +1,7 @@
 package io.github.jgabrielfreitas.stonewars.service.api
 
 import io.github.jgabrielfreitas.stonewars.BuildConfig.STONE_WARS_API
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 /**
@@ -9,10 +10,14 @@ import retrofit2.Retrofit
 
 class RetrofitHelper {
 
-    fun createRetrofitInstance(url: String = STONE_WARS_API): Retrofit {
+    fun createRetrofitInstance(url: String = STONE_WARS_API, client: OkHttpClient = createClientHttp()): Retrofit {
         return Retrofit.Builder()
-                       .baseUrl(url)
-                       .build()
+                .baseUrl(url)
+                .build()
+    }
+
+    private fun createClientHttp(): OkHttpClient {
+        return OkHttpClient.Builder().build()
     }
 
 }
